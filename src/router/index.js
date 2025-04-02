85f809d4a0b554e002a448a35b08a6382693d1e0
AKADortys <thibault.a@outlook.com>
2025-04-02 04:43:29 +0200

    Adaptation pour githubpages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
 import Dashboard from '@/views/Profile.vue'
@@ -14,7 +14,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(), //history: createWebHistory(),
   routes,
 })
 
